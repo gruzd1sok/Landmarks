6bb65d14fdd2100e71d2a159409770edcaafab36
Ilya Gruzdev <ily.gruzdev99@gmail.com>
2021-06-10 11:37:13 +0700

    end
    end tutorial

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Илья Груздев on 09.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
---
+++ WatchLandmarks Extension/LandmarkDetail.swift
@@ -0,0 +1,59 @@
+//
+//  LandmarkDetail.swift
+//  WatchLandmarks Extension
+//
+//  Created by Илья Груздев on 09.06.2021.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFill()
+                
+                Text(landmark.name)
+                    .font(.headline)
+                    .lineLimit(0)
+                
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
+                    Text("Favorite")
+                }
+                
+                Divider()
+                
+                Text(landmark.park)
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+                
+                Text(landmark.state)
+                    .font(.caption)
+            }
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        let modelData = ModelData()
+        return Group {
+                    LandmarkDetail(landmark: modelData.landmarks[0])
+                        .environmentObject(modelData)
+                        .previewDevice("Apple Watch Series 5 - 44mm")
+
+                    LandmarkDetail(landmark: modelData.landmarks[1])
+                        .environmentObject(modelData)
+                        .previewDevice("Apple Watch Series 5 - 40mm")
+                }
+    }
+}
